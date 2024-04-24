1546e1032c43777c263ac2c64b358aaebada82fe
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-04-24 16:00:15 +0100

    dup wdiget

--- template_selector.py
+++ template_selector.py
@@ -85,7 +85,7 @@ def main():
     edit_csv_df.loc[edit_csv_df['Ad type'] == 'Responsive search ad', 'Final URL'] = final_url
 
     edit_csv_df_ad_editor_csv = template_retrieval.convert_df(edit_csv_df)
-    st.download_button("Press to Download",csv_df_ad_editor_csv, csv_name+"_edited_template.csv","text/csv",key='download-edited_csv')
+    st.download_button("Press to Download",edit_csv_df_ad_editor_csv, csv_name+"_edited_template.csv","text/csv",key='download-edited_csv')
     
     
        
