4c7bc1386fffdef3fcfc8866fa7417faa17e23fa
addpeoplemain <167210268+addpeoplemain@users.noreply.github.com>
2024-04-24 14:53:38 +0100

    reoorder

--- template_selector.py
+++ template_selector.py
@@ -21,7 +21,25 @@ import template_retrieval
 
 
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
-def main():
+def main():    
+    st.header("Ad editor template application 1.0")
+
+    st.write("The current proof of concept demonstrator can be used select csv templates to import into google ads editor" )
+    st.write("Use the side bar on the left to select: the desired template" )
+
+    if choice == choice_value:
+        st.title(choice_value +"Template")
+        csv_name = choice_value.lower()+"_test.csv"
+        csv_name= "_".join( csv_name.split())
+        st.subheader(csv_name)
+        csv_df = pd.read_csv(csv_name)
+        st.dataframe(csv_df)
+        csv_df_ad_editor_csv = template_retrieval.convert_df(csv_df)
+        st.download_button("Press to Download",csv_df_ad_editor_csv, csv_name+"_template.csv","text/csv",key='download-csv')
+
+    st.title("Edited Template")
+    edit_csv_df = csv_df.copy()
+
     menu =["Tree Surgery","Interior Design"]
     with st.sidebar:
       #  url_search = st.text_input("//TODO Database URL")
@@ -42,24 +60,6 @@ def main():
         final_url_input= st.number_input("Enter Final URL for Ads",key = "final_url")
         if st.button('Set Final URL'):
             final_url = final_url_input
-       
-    st.header("Ad editor template application 1.0")
-
-    st.write("The current proof of concept demonstrator can be used select csv templates to import into google ads editor" )
-    st.write("Use the side bar on the left to select: the desired template" )
-
-    if choice == choice_value:
-        st.title(choice_value +"Template")
-        csv_name = choice_value.lower()+"_test.csv"
-        csv_name= "_".join( csv_name.split())
-        st.subheader(csv_name)
-        csv_df = pd.read_csv(csv_name)
-        st.dataframe(csv_df)
-        csv_df_ad_editor_csv = template_retrieval.convert_df(csv_df)
-        st.download_button("Press to Download",csv_df_ad_editor_csv, csv_name+"_template.csv","text/csv",key='download-csv')
-
-    st.title("Edited Template")
-    edit_csv_df = csv_df.copy()
   
 if __name__ == "__main__":
     main()
